757431871f3d65853f9f1b12568f37a273927c39
unknown <andreabcm16@gmail.com>
2021-06-04 09:51:54 +0200

    created events

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -43,9 +43,33 @@
                             <button>Cancel</button>
                         </div>
                     </li>
+
+                    <li class="event-card inline-flex border-2 border-indigo-900 mx-3 my-3" style="width: 86vw">
+                        <div class="plate"><p class="mx-7 my-4 font-bold text-lg" style="color: #00B3D5">SQL/ <br> PHP</p></div>
+                        <div class="event-description flex flex-col">
+                            <date class="flex justify-end mr-2">03/06/2021</date>
+                            <h2 class="font-bold text-2xl">Google API</h2>
+                            <p style="background-color: #D6FEF6; width: 10.2rem">29 places / 1 available</p>
+                            <p>These APIs allow the <br>users to get information...</p>
+                            <a>Read More</a>
+                            <button>Cancel</button>
+                        </div>
+                    </li>
+
+                    <li class="event-card inline-flex border-2 border-indigo-900 mx-3 my-3" style="width: 86vw">
+                        <div class="plate"><p class="mx-7 my-4 font-bold text-lg" style="color: #00B3D5">SQL/ <br> PHP</p></div>
+                        <div class="event-description flex flex-col">
+                            <date class="flex justify-end mr-2">03/06/2021</date>
+                            <h2 class="font-bold text-2xl">Google API</h2>
+                            <p style="background-color: #D6FEF6; width: 10.2rem">29 places / 1 available</p>
+                            <p>These APIs allow the <br>users to get information...</p>
+                            <a>Read More</a>
+                            <button>Cancel</button>
+                        </div>
+                    </li>
                 </ul>
 
-                <nav>icons</nav>
+                <nav></nav>
             </main>
         </div>
 
