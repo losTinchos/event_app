dfc96c96ef09006388e5ddc29f5db566c730adc6
solomonclark <you@example.com>
2021-06-07 14:30:13 +0200

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 <x-guest-layout>
-    <x-auth-card>
+    <x-auth-card >
         <x-slot name="logo">
             <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png"class=" h-44 fill-current text-gray-500 items-center m-20 mb-0" />
             </a>
         </x-slot>
 
@@ -40,17 +40,21 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-center flex-col mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-md text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
-
-                <x-button class="ml-3">
+                <a class="underline text-md text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    {{ __('register?') }}
+                </a>
+                <x-button class="ml-3 mt-40 bg-blue-light ">
                     {{ __('Log in') }}
                 </x-button>
+
             </div>
+                
         </form>
     </x-auth-card>
 </x-guest-layout>
