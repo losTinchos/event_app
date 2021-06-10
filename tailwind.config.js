851530a326aba1124a9534d9fe391a96a192e9b0
Diego Gonzalez Valenzuela <dgonzvdev@gmail.com>
2021-06-10 10:38:53 +0200

    tailwind config error corrected

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,31 @@
 module.exports = {
-  purge: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
-  ],
+    purge: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
 
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        blue: {
-            dark: "#291C60",
-            light: "#00B3D5",
-            DEFAULT: "#0068AE",
-        },
-        aqua: {
-            DEFAULT: "#56FBDB",
-            light: "#D6FEF6",
-        },
-        yellow: {
-            DEFAULT: "#F9F871",
-        },
-        red: {
-            DEFAULT: "#92201C",
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                blue: {
+                    dark: "#291C60",
+                    light: "#00B3D5",
+                    DEFAULT: "#0068AE",
+                },
+                aqua: {
+                    DEFAULT: "#56FBDB",
+                    light: "#D6FEF6",
+                },
+                yellow: {
+                    DEFAULT: "#F9F871",
+                },
+                red: {
+                    DEFAULT: "#92201C",
+                },
+            },
+            plugins: [],
         },
     },
-    plugins: [],
 };
